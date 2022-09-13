a1757e573ac1807c9a374609bf83cb8c15d57cdf
Romain J <romain.ordi@gmail.com>
2022-09-13 23:13:59 +0200

    style

--- config/settings/base.py
+++ config/settings/base.py
@@ -372,7 +372,4 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     "http://127.0.0.1:8080",
     "http://127.0.0.1:8081",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8001"
-]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8080", "http://127.0.0.1:8001"]
